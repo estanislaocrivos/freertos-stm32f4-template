c0ca5959d1a1dc4d4e38b3b143d7481dc3e79e03
estanislaocrivos <estanislaocrivos@gmail.com>
2026-02-07 12:25:57 -0300

    docs: minor changes

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -8,79 +8,62 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
-/*-----------------------------------------------------------
- * Basic system configuration
- *----------------------------------------------------------*/
-
 /* CPU frequency in Hz.
  * STM32F401 can run up to 84MHz.
  * By default it boots with HSI at 16MHz. */
-#define configCPU_CLOCK_HZ              (16000000UL)
+#define configCPU_CLOCK_HZ                  (16000000UL)
 
 /* RTOS tick frequency (interrupts per second).
  * 1000 = tick every 1ms. Typical values: 100-1000 Hz */
-#define configTICK_RATE_HZ              ((TickType_t)1000)
+#define configTICK_RATE_HZ                  ((TickType_t)1000)
 
 /* Task priority levels (0 = lowest) */
-#define configMAX_PRIORITIES            (5)
+#define configMAX_PRIORITIES                (5)
 
 /* Minimum stack size per task (in words, not bytes).
  * 128 words = 512 bytes. For Cortex-M4 one word = 4 bytes */
-#define configMINIMAL_STACK_SIZE        ((uint16_t)128)
+#define configMINIMAL_STACK_SIZE            ((uint16_t)128)
 
 /* FreeRTOS heap size in bytes.
  * Memory for tasks, queues, semaphores, etc. is allocated from here. */
-#define configTOTAL_HEAP_SIZE           ((size_t)(32 * 1024))
+#define configTOTAL_HEAP_SIZE               ((size_t)(32 * 1024))
 
 /* Maximum task name length */
-#define configMAX_TASK_NAME_LEN         (16)
+#define configMAX_TASK_NAME_LEN             (16)
 
 /* Use 32-bit tick counter (vs 16-bit) */
-#define configUSE_16_BIT_TICKS          0
+#define configUSE_16_BIT_TICKS              0
 
 /* Allow tasks of equal priority to share CPU time */
-#define configUSE_PREEMPTION            1
-#define configUSE_TIME_SLICING          1
+#define configUSE_PREEMPTION                1
+#define configUSE_TIME_SLICING              1
 
 /* Hook functions (optional callbacks) */
-#define configUSE_IDLE_HOOK             0
-#define configUSE_TICK_HOOK             0
-#define configUSE_MALLOC_FAILED_HOOK    0
-#define configCHECK_FOR_STACK_OVERFLOW  0
-
-/*-----------------------------------------------------------
- * Enabled features
- *----------------------------------------------------------*/
+#define configUSE_IDLE_HOOK                 0
+#define configUSE_TICK_HOOK                 0
+#define configUSE_MALLOC_FAILED_HOOK        0
+#define configCHECK_FOR_STACK_OVERFLOW      0
 
 /* Mutexes and semaphores */
-#define configUSE_MUTEXES               1
-#define configUSE_RECURSIVE_MUTEXES     1
-#define configUSE_COUNTING_SEMAPHORES   1
+#define configUSE_MUTEXES                   1
+#define configUSE_RECURSIVE_MUTEXES         1
+#define configUSE_COUNTING_SEMAPHORES       1
 
 /* Software timers */
-#define configUSE_TIMERS                1
-#define configTIMER_TASK_PRIORITY       (configMAX_PRIORITIES - 1)
-#define configTIMER_QUEUE_LENGTH        10
-#define configTIMER_TASK_STACK_DEPTH    (configMINIMAL_STACK_SIZE * 2)
+#define configUSE_TIMERS                    1
+#define configTIMER_TASK_PRIORITY           (configMAX_PRIORITIES - 1)
+#define configTIMER_QUEUE_LENGTH            10
+#define configTIMER_TASK_STACK_DEPTH        (configMINIMAL_STACK_SIZE * 2)
 
 /* Queue sets and task notifications */
-#define configUSE_QUEUE_SETS            1
-#define configUSE_TASK_NOTIFICATIONS    1
-
-/*-----------------------------------------------------------
- * Memory configuration
- *----------------------------------------------------------*/
+#define configUSE_QUEUE_SETS                1
+#define configUSE_TASK_NOTIFICATIONS        1
 
 /* Use dynamic memory allocation (pvPortMalloc/vPortFree) */
 #define configSUPPORT_DYNAMIC_ALLOCATION    1
 
 /* Don't use static allocation for now */
 #define configSUPPORT_STATIC_ALLOCATION     0
-
-/*-----------------------------------------------------------
- * Optional API functions
- * 1 = include, 0 = exclude (saves Flash)
- *----------------------------------------------------------*/
 
 #define INCLUDE_vTaskPrioritySet            1
 #define INCLUDE_uxTaskPriorityGet           1
@@ -93,16 +76,12 @@
 #define INCLUDE_uxTaskGetStackHighWaterMark 1
 #define INCLUDE_xTaskGetIdleTaskHandle      1
 
-/*-----------------------------------------------------------
- * Cortex-M specific configuration
- *----------------------------------------------------------*/
-
 /* Cortex-M4 has 4 priority bits (16 levels).
  * ST uses the 4 most significant bits. */
 #ifdef __NVIC_PRIO_BITS
-    #define configPRIO_BITS __NVIC_PRIO_BITS
+#define configPRIO_BITS __NVIC_PRIO_BITS
 #else
-    #define configPRIO_BITS 4
+#define configPRIO_BITS 4
 #endif
 
 /* Lowest interrupt priority */
@@ -119,14 +98,6 @@
 
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY \
     (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
-
-/*-----------------------------------------------------------
- * Kernel handlers
- *
- * FreeRTOS needs to handle these Cortex-M exceptions.
- * We rename them to match the names in ST's startup code
- * vector table.
- *----------------------------------------------------------*/
 
 #define vPortSVCHandler     SVC_Handler
 #define xPortPendSVHandler  PendSV_Handler
